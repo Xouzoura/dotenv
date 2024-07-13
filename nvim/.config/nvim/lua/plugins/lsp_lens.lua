ae7899d2395a124b6b96cd82f25c74f1c9a98537
xouzoura <dimisotiris@yahoo.com>
2024-07-13 16:18:49 +0200

    seriously last

--- nvim/.config/nvim/lua/plugins/lsp_lens.lua
+++ nvim/.config/nvim/lua/plugins/lsp_lens.lua
@@ -8,7 +8,10 @@ return {
       include_declaration = false, -- Reference include declaration
       sections = { -- Enable / Disable specific request, formatter example looks 'Format Requests'
         definition = false,
-        references = true,
+        references = function(count)
+          return "...RefCount: [" .. count .. "]"
+        end,
+        -- references = true,
         implements = true,
         git_authors = false,
       },
