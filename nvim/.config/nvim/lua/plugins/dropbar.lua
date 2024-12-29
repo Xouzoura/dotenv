0247d32d24ec23a7253b5fabd28b555681ed86bd
xouzoura <dimisotiris@yahoo.com>
2024-12-29 16:42:25 +0100

    re-enable dropbar

--- nvim/.config/nvim/lua/plugins/dropbar.lua
+++ nvim/.config/nvim/lua/plugins/dropbar.lua
@@ -1,7 +1,7 @@
 return {
   -- The bar on the top of the file that shows the current function/directory
   "Bekaboo/dropbar.nvim",
-  enabled = false,
+  -- enabled = false,
   lazy = false,
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
