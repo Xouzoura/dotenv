ad19b43955328c75a5ab481c1b01cd16eb79ca10
xouzoura <dimisotiris@yahoo.com>
2025-04-29 10:23:41 +0200

    feat: update setup to include kitty

--- setup.sh
+++ setup.sh
@@ -81,12 +81,30 @@ fi
 
 # Optional but why not 
 if [ "$EXTRAS" == true ]; then
+    # Decide between wezterm and kitty
+    # (
+    #     # Wezterm, currently use kitty 
+    #     curl -LO https://github.com/wezterm/wezterm/releases/download/20240203-110809-5046fc22/WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
+    #     chmod +x WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
+    #     mv WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage ~/.local/bin/wezterm
+    #     echo "Wezterm installed successfully at version $(wezterm --version)"
+    # )
     (
-        # Wezterm 
-        curl -LO https://github.com/wezterm/wezterm/releases/download/20240203-110809-5046fc22/WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
-        chmod +x WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
-        mv WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage ~/.local/bin/wezterm
-        echo "Wezterm installed successfully at version $(wezterm --version)"
+        curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+        echo "Kitty installed successfully at version $(kitty --version)"
+        # Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
+        # your system-wide PATH)
+        ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+        # Place the kitty.desktop file somewhere it can be found by the OS
+        cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+        # If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+        cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+        # Update the paths to the kitty and its icon in the kitty desktop file(s)
+        sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+        sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+        # Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
+        echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+        echo "Kitty desktop installed successfully."
     )
     (
         # Yazi
