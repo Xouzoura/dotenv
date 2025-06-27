f987d465abc2d5d56890831deee52284fa4af904
xouzoura <dimisotiris@yahoo.com>
2025-06-27 09:49:16 +0200

    feat: rename file and fix small stuff in the backup

---
+++ scripts/backup_home_folder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+choice="${1:-local}"
+
+if [ "$choice" != "local" ] && [ "$choice" != "external" ]; then
+  echo "Usage: $0 [local|external]"
+  exit 1
+fi
+
+SOURCE_DIR="$HOME"
+LOCAL_DIR="/home/$USER/Koofr/backups/home/$USER"
+EXTERNAL_DISK="T7 Touch"
+EXTERNAL_DIR="/media/$USER/$EXTERNAL_DISK/backups/home/$USER"
+PACKAGES_DIR="/home/$USER/packages"
+
+if [ "$choice" == "local" ]; then
+    DEST_DIR="$LOCAL_DIR"
+else
+    DEST_DIR="$EXTERNAL_DIR"
+fi
+
+mkdir -p "$DEST_DIR"
+mkdir -p $PACKAGES_DIR
+dpkg --get-selections > $PACKAGES_DIR/ubuntu-packages.txt
+snap list > $PACKAGES_DIR/snap-packages.txt
+flatpak list > $PACKAGES_DIR/flatpak-packages.txt
+rsync -av --progress --exclude-from="exclude.txt" "$SOURCE_DIR" "$DEST_DIR"
+echo "$(date) Synced $SOURCE_DIR to $DEST_DIR" >> "$DEST_DIR/rsync-home.log"
--- scripts/backup_last_snapshot.sh
+++ scripts/backup_last_snapshot.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 # WARNING: requires sudo ;)
 
+choice="${1:-local}"
+
+if [ "$choice" != "local" ] && [ "$choice" != "external" ]; then
+  echo "Usage: $0 [local|external]"
+  exit 1
+fi
+
 SOURCE_DIR="/timeshift/snapshots"
 
 DEST_DIR="/home/$USER/Koofr/backups/ubuntu-snapshots"
 EXTERNAL_DISK="T7 Touch"
 EXTERNAL_DIR="/media/$USER/$EXTERNAL_DISK/backups/ubuntu-snapshots"
 
-if [ $# -eq 0 ]; then
+if [ "$choice" == "local" ]; then
     # Local
     DEST_DIR="$DEFAULT_DIR"
 else
