c871efb50b36c7d27f5ed0d6f80648f70fbb3dc1
xouzoura <dimisotiris@yahoo.com>
2025-09-20 11:27:52 +0200

    feat: wezterm support and make it like kitty for windows

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,17 +5,23 @@ return {
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	font = wezterm.font("JetBrains Mono"), -- Set your favorite font
 	color_scheme = "Dracula", -- Use a built-in color scheme
+	colors = {
+		cursor_fg = "#111111",
+		cursor_bg = "#FF00FF",
+	},
 	window_background_opacity = 1, -- 0.98, -- Transparency
-	enable_tab_bar = true, -- Show tabs
-	use_fancy_tab_bar = false, -- Use minimal tab style
+	font_size = 12.0,
+	enable_tab_bar = true,
+	use_fancy_tab_bar = false,
 	enable_wayland = true,
-	tab_bar_at_bottom = false, -- Ensure the tab bar is at the top
+	tab_bar_at_bottom = false,
 	-- window_decorations = "NONE",
 	window_decorations = "RESIZE",
 	hide_tab_bar_if_only_one_tab = false,
 	warn_about_missing_glyphs = false,
 	use_resize_increments = false,
 	adjust_window_size_when_changing_font_size = false,
+	hide_mouse_cursor_when_typing = true,
 	window_padding = {
 		left = 0,
 		right = 0,
@@ -29,23 +35,24 @@ return {
 	-- leader = { key = "Space", mods = "SHIFT" },
 	leader = { key = "q", mods = "CTRL" },
 	default_prog = { "zsh" }, -- Change this to your preferred shell if necessary
-	hide_mouse_cursor_when_typing = true,
 	keys = {
-		-- Close the current tab
-		{ key = "Tab", mods = "CTRL", action = "DisableDefaultAssignment" },
-		{ key = "Tab", mods = "CTRL|SHIFT", action = "DisableDefaultAssignment" },
-
-		-- Custom tab switching: Leader + Tab to move to the next tab
-		{ key = "Tab", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
-
-		-- Switch to specific tabs with Leader + number (1, 2, 3...)
-		{ key = "1", mods = "LEADER", action = wezterm.action.ActivateTab(0) },
-		{ key = "2", mods = "LEADER", action = wezterm.action.ActivateTab(1) },
-		{ key = "3", mods = "LEADER", action = wezterm.action.ActivateTab(2) },
+		{ key = ".", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) },
+		{ key = ",", mods = "ALT", action = wezterm.action.ActivateTabRelative(-1) },
+		{ key = "'", mods = "ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "h", mods = "ALT|SHIFT", action = wezterm.action.ActivatePaneDirection("Left") },
+		{ key = "l", mods = "ALT|SHIFT", action = wezterm.action.ActivatePaneDirection("Right") },
+		{ key = "k", mods = "ALT|SHIFT", action = wezterm.action.ActivatePaneDirection("Up") },
+		{ key = "j", mods = "ALT|SHIFT", action = wezterm.action.ActivatePaneDirection("Down") },
+		{ key = "o", mods = "ALT|SHIFT", action = wezterm.action.ActivatePaneDirection("Next") },
+		{ key = ";", mods = "ALT", action = act.SpawnTab("CurrentPaneDomain") },
+		{ key = "[", mods = "ALT", action = act.ActivateCopyMode },
 		{
-			key = "p",
-			mods = "LEADER",
-			action = act.PasteFrom("Clipboard"),
+			key = "r",
+			mods = "CTRL|SHIFT",
+			action = act.ActivateKeyTable({
+				name = "resize_pane",
+				one_shot = false,
+			}),
 		},
 		{
 			key = "t",
@@ -53,63 +60,9 @@ return {
 			action = wezterm.action.SpawnTab("CurrentPaneDomain"),
 		},
 		{
-			key = "z",
-			mods = "LEADER",
-			action = act.TogglePaneZoomState,
-		},
-		{
 			key = "w",
 			mods = "CMD",
 			action = act.CloseCurrentPane({ confirm = true }),
-		},
-		{
-			key = "v",
-			mods = "LEADER",
-			action = act.ActivateCopyMode,
-		},
-
-		-- activate resize mode
-		{
-			key = "r",
-			mods = "LEADER",
-			action = act.ActivateKeyTable({
-				name = "resize_pane",
-				one_shot = false,
-			}),
-		},
-
-		-- focus panes
-		{
-			key = "h",
-			mods = "LEADER",
-			action = act.ActivatePaneDirection("Left"),
-		},
-		{
-			key = "l",
-			mods = "LEADER",
-			action = act.ActivatePaneDirection("Right"),
-		},
-		{
-			key = "k",
-			mods = "LEADER",
-			action = act.ActivatePaneDirection("Up"),
-		},
-		{
-			key = "j",
-			mods = "LEADER",
-			action = act.ActivatePaneDirection("Down"),
-		},
-
-		-- add new panes
-		{
-			key = "-",
-			mods = "LEADER",
-			action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "\\",
-			mods = "LEADER",
-			action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 		},
 	},
 
