b785612395910a4ce897279d4c3bca66d741e198
xouzoura <dimisotiris@yahoo.com>
2025-09-28 12:01:41 +0200

    feat: add autosession

---
+++ nvim/.config/nvim/lua/plugins/autosession.lua
@@ -0,0 +1,18 @@
+return {
+  "rmagatti/auto-session",
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+    autosave = false,
+    -- log_level = 'debug',
+  },
+  keys = {
+    { "<leader>ss", "<cmd>AutoSession save<CR>", desc = "<auto-session> Save session" },
+    { "<leader>sr", "<cmd>AutoSession delete<CR>", desc = "<auto-session> Delete session" },
+    { "<leader>sf", "<cmd>AutoSession search<CR>", desc = "<auto-session> Search session" },
+  },
+}
---
+++ totry.md
@@ -0,0 +1,3 @@
+# What I want to try at one point and too lazy now
+
+- [ ] smart-splits.nvim
