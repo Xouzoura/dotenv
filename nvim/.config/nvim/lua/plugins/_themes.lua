f8e9deae9028e02c3f0090cfc84e93a21cb0ad89
xouzoura <dimisotiris@yahoo.com>
2025-10-11 12:34:04 +0200

    feat: small config of nightfox

--- nvim/.config/nvim/lua/plugins/_themes.lua
+++ nvim/.config/nvim/lua/plugins/_themes.lua
@@ -1,1 +1,28 @@
-return { { "EdenEast/nightfox.nvim" } }
+return {
+  "EdenEast/nightfox.nvim",
+  config = function()
+    require("nightfox").setup {
+      options = {
+        styles = {
+          keywords = "bold",
+          types = "bold",
+        },
+      },
+      palettes = {
+        all = {
+          red = "#ff0000",
+        },
+        nightfox = {
+          red = "#c94f6d",
+        },
+        dayfox = {
+          blue = { base = "#4d688e", bright = "#4e75aa", dim = "#485e7d" },
+        },
+        nordfox = {
+          sel1 = "#4f6074", -- Popup sel bg, search bg
+          comment = "#60728a",
+        },
+      },
+    }
+  end,
+}
