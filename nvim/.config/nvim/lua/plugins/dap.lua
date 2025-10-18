925a4e1d3531919a9901748b458c2466791010d5
xouzoura <dimisotiris@yahoo.com>
2025-10-18 15:56:05 +0200

    chore: remove print

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -76,7 +76,6 @@ return {
         function()
           for _, win in ipairs(vim.api.nvim_list_wins()) do
             local buf = vim.api.nvim_win_get_buf(win)
-            print(vim.api.nvim_buf_get_name(buf))
             if vim.api.nvim_buf_get_name(buf):match "Console" or vim.api.nvim_buf_get_name(buf):match "terminal" then
               vim.api.nvim_set_current_win(win)
               return
