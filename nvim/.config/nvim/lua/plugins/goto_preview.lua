7b186a4b6111e0f8ad6cc7446d4a104124682377
xouzoura <dimisotiris@yahoo.com>
2025-10-25 14:09:47 +0200

    feat: add control-space for viewing definition

--- nvim/.config/nvim/lua/plugins/goto_preview.lua
+++ nvim/.config/nvim/lua/plugins/goto_preview.lua
@@ -1,17 +1,6 @@
 return {
-  -- {
-  --   -- preview with gp
-  --   "rmagatti/goto-preview",
-  --   lazy = false,
-  --   config = function()
-  --     require("goto-preview").setup {
-  --       default_mappings = true,
-  --       -- mappings: gp + d,D,i,r,t
-  --     }
-  --   end,
-  -- },
   {
-    -- preview with glance. <leader>l to go to definition
+    -- preview with definitions etc
     "dnlhc/glance.nvim",
     lazy = false,
     config = function()
@@ -27,6 +16,7 @@ return {
       }
       vim.keymap.set("n", "gpd", "<CMD>Glance definitions<CR>")
       vim.keymap.set("n", "gpr", "<CMD>Glance references<CR>")
+      vim.keymap.set("n", "<c-space>", "<CMD>Glance definitions<CR>")
       -- vim.keymap.set("n", "gpt", "<CMD>Glance type_definitions<CR>")
       -- vim.keymap.set("n", "gpi", "<CMD>Glance implementations<CR>")
     end,
