74ca1db7899566f74fd3aa4a0526ca9c093f040a
xouzoura <dimisotiris@yahoo.com>
2026-02-08 09:10:11 +0100

    feat: switch to ty instead of lsp

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -17,8 +17,17 @@ vim.lsp.config("pyright", {
     },
   },
 })
-local servers = { "html", "cssls", "clangd", "lua_ls", "ts_ls", "angularls", "taplo", "pyright", "csharp_ls" }
+
+-- <PICK SERVERS> --
+
+-- 1) old with pyright
+-- local servers = { "html", "cssls", "clangd", "lua_ls", "ts_ls", "angularls", "taplo", "pyright", "csharp_ls"} -- pyright
+-- 2) experimental with ty
+local servers = { "html", "cssls", "clangd", "lua_ls", "ts_ls", "angularls", "taplo", "csharp_ls", "ty" } -- ty
+-- 3) super basic with pyright
 -- local servers = { "html", "clangd", "taplo", "pyright" } -- simpler
+
+-- << enable server >>
 for _, lsp in ipairs(servers) do
   vim.lsp.enable(lsp)
 end
