5d278f5a4ede240e12278f5fad5d290abe5dba3b
xouzoura <dimisotiris@yahoo.com>
2026-06-12 21:03:25 +0200

    feat: add yaml support lsp

---
+++ nvim/.config/nvim/lua/plugins/yaml.lua
@@ -0,0 +1,15 @@
+return {
+  "mosheavni/yaml-companion.nvim",
+  lazy = false,
+  opts = {
+    -- Add any options here, or leave empty to use the default settings
+    -- lspconfig = {
+    --   settings = { ... }
+    -- },
+  },
+  config = function(_, opts)
+    local cfg = require("yaml-companion").setup(opts)
+    vim.lsp.config("yamlls", cfg)
+    vim.lsp.enable "yamlls"
+  end,
+}
