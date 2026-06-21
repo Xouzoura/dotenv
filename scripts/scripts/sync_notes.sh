175efb50eb457b8bb48c312a6411c4e10b0fe213
xouzoura <dimisotiris@yahoo.com>
2026-06-21 18:02:14 +0200

    sync note thingy update to avoid when editing too many commits

--- scripts/scripts/sync_notes.sh
+++ scripts/scripts/sync_notes.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
-
+if pgrep -a nvim | grep -q "vaults/notes"; then
+    echo "[sync] nvim session found, so skipping..."
+    exit 0
+fi
 cd /home/xouzoura/vaults/notes || exit 1
 if ! curl -fsS --max-time 3 https://github.com >/dev/null; then
   echo "[sync] no internet, skipping"
