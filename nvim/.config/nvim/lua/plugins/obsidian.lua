1aacd9255ee0085f7751d4f1f9f53d7b5eea7a92
xouzoura <dimisotiris@yahoo.com>
2026-07-12 12:42:45 +0200

    try all directories

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -92,6 +92,19 @@ return {
         name = "personal",
         path = "~/vaults/personal",
       },
+      {
+        name = "no-vault",
+        path = function()
+          return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+        end,
+        overrides = {
+          notes_subdir = vim.NIL,
+          new_notes_location = "current_dir",
+          templates = {
+            folder = vim.NIL,
+          },
+        },
+      },
       -- {
       --   name = "work",
       --   path = "~/vaults/work",
